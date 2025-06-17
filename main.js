9157e6dd6125aae40ff4a0235bdbe30d2ef20350
adel.belkercha <adel.belkercha@epita.fr>
2025-06-17 22:30:13 +0200

    feat: first commit game drawing to a canvas some sprites with a great reusable base thanks to a tutorial

---
+++ main.js
@@ -0,0 +1,53 @@
+import {resources} from "./src/Resource.js"
+import { Sprite } from "./src/Sprite.js";
+import { Vector2 } from "./src/Vector2.js";
+
+const canvas = document.querySelector('#game-canvas');
+const ctx = canvas.getContext("2d");
+
+const skySprite = new Sprite({
+    resource: resources.images.sky,
+    frameSize: new Vector2(320, 180)
+});
+
+const groundSprite = new Sprite({
+    resource: resources.images.ground,
+    frameSize: new Vector2(320, 180)
+})
+
+const hero = new Sprite({
+    resource: resources.images.hero,
+    frameSize: new Vector2(32,32),
+    hFrames: 3,
+    vFrames: 8,
+    frame: 1
+})
+
+const shadow = new Sprite({
+    resource: resources.images.shadow,
+    frameSize: new Vector2(32,32)
+})
+
+const heroPos = new Vector2(16 * 6, 16 * 5);
+
+const draw = () => {
+    skySprite.drawImage(ctx, 0, 0);
+    groundSprite.drawImage(ctx, 0, 0);
+
+    const heroOffset = new Vector2(-8, -20);
+    const heroPosX = heroPos.x+heroOffset.x;
+    const heroPosY = heroPos.y+heroOffset.y;
+    shadow.drawImage(ctx, heroPosX, heroPosY);
+    hero.drawImage(ctx, heroPosX, heroPosY);
+}
+
+
+setInterval(() => {
+    // temp draw loop
+    if (hero.frame < 24) {
+        hero.frame++;
+    } else {
+        hero.frame = 0;
+    }
+    draw()
+}, 300)
