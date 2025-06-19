7bd57718b810f659b162e8f71fd782e2f5a2160e
adel.belkercha <adel.belkercha@epita.fr>
2025-06-19 20:13:16 +0200

    feat: level1 done collisions added manually -> todo todo: make a level design tool to make collisions automatically

--- src/levels/level1.js
+++ src/levels/level1.js
@@ -1,13 +1,47 @@
 export const walls = new Set();
 
-walls.add(`64,48`); // tree
+function addWall(x,y) {
+  walls.add(`${x*16},${y*16}`);
+}
 
-walls.add(`64,64`); // squares
-walls.add(`64,80`);
-walls.add(`80,64`);
-walls.add(`80,80`);
+function addVertical(x,y,length) {
+  for (let i = 0; i < length; i++) {
+    addWall(x,y-i);
+  }
+}
 
-walls.add(`112,80`); // water
-walls.add(`128,80`);
-walls.add(`144,80`);
-walls.add(`160,80`);
+function addHorizontal(x,y,length) {
+  for (let i = 0; i < length; i++) {
+    addWall(x+i,y);
+  }
+}
+
+// trees
+addWall(4,3);
+addWall(13,4);
+addWall(14,2);
+
+// squares
+addWall(4,4);
+addWall(4,5);
+addWall(5,4);
+addWall(5,5);
+addWall(8,3);
+addWall(9,3);
+
+// water line
+addHorizontal(7,5,4);
+
+// rocks
+addHorizontal(12,6,3);
+
+// house
+addWall(14,4);
+
+// bounding box
+addHorizontal(2,7,15); // down
+addVertical(16,7,6); // vert-left
+addVertical(2,7,6); // vert-right
+addHorizontal(3,2,4); // up-right
+addWall(15,2); // up-left
+addHorizontal(6,1,8); // top
